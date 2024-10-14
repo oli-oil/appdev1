f2a13a08af7abb8e80b9a54ce1e4a83d61063e28
oli-oil <princessolingay@gmail.com>
2024-10-14 19:13:32 +0800

    Responding to events DONE ^^

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,25 +1,14 @@
-const products = [
-    { title: 'Cabbage', isFruit: false, id: 1 },
-    { title: 'Garlic', isFruit: false, id: 2 },
-    { title: 'Apple', isFruit: true, id: 3 },
-  ];
-  
-  export default function ShoppingList() {
-    const listItems = products.map(product =>
-      <li
-        key={product.id}
-        style={{
-          color: product.isFruit ? 'magenta' : 'darkgreen'
-        }}
-      >
-        {product.title}
-      </li>
-    );
-  
-    return (
-      <ul>{listItems}</ul>
-    );
+function MyButton() {
+  function handleClick() {
+    alert('You clicked me!');
   }
+
+  return (
+    <button onClick={handleClick}>
+      Click me
+    </button>
+  );
+}
+
+export default MyButton 
    
-
-
