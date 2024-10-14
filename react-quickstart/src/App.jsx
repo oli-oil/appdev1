cdd73ec8b56bf60038e22350b6d7042fac91e8a2
oli-oil <princessolingay@gmail.com>
2024-10-14 19:15:18 +0800

    Counters that update separately DONEEEE

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,14 +1,27 @@
+import { useState } from 'react';
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Counters that update separately</h1>
+      <MyButton />
+      <MyButton />
+    </div>
+  );
+}
+
 function MyButton() {
+  const [count, setCount] = useState(0);
+
   function handleClick() {
-    alert('You clicked me!');
+    setCount(count + 1);
   }
 
   return (
     <button onClick={handleClick}>
-      Click me
+      Clicked {count} times
     </button>
   );
 }
 
-export default MyButton 
    
