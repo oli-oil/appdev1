4f3d357bb3af93c8fe8b7908204b332d7f5bb137
oli-oil <princessolingay@gmail.com>
2024-10-14 18:56:21 +0800

    Displaying data done XXD

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,13 +1,26 @@
 
-function MyApp() {
-  return (
-    <>
-      <h1>An Image</h1>
-      <img className="avatar" />
-    </>
-  );
+export default function Profile() {
+    const user = {
+      name: 'Hedy Lamarr',
+      imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+      imageSize: 90,
+    }
+    
+    return (
+      <>
+        <h1>{user.name}</h1>
+        <img
+          className="avatar"
+          src={user.imageUrl}
+          alt={'Photo of ' + user.name}
+          style={{
+            width: user.imageSize,
+            height: user.imageSize
+          }}
+        />
+      </>
+    
+    );
 }
-
-export default MyApp
 
 
